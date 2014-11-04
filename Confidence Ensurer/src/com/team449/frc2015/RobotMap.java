eb69397b190d2d3352a8389d561f68a55e8b98e4
hazheng <hazheng@mbhs.edu>
2014-11-04 17:09:40 -0500

    Added comments and roller and flinger constants
    Added comments for the drivebase components
    Added flinger solenoid
    Added roller solenoids and roller motor ports

--- Confidence Ensurer/src/com/team449/frc2015/RobotMap.java
+++ Confidence Ensurer/src/com/team449/frc2015/RobotMap.java
@@ -7,12 +7,21 @@ package com.team449.frc2015;
  * floating around.
  */
 public class RobotMap {
+    //Drivebase PWM ports
     public static final int left1motor = 1;
     public static final int left2motor = 2;
     public static final int left3motor = 3;
     public static final int right1motor = 4;
     public static final int right2motor = 5;
     public static final int right3motor = 6;
+    
+    //Constants for the flinger
+    public static final int flinger_sol = 1; //flinger solenoid port
+    
+    //constants for the roller
+    public static final int roller_sol_1 = 2; //roller solenoid ports (double stage solenoid)
+    public static final int roller_sol_2 = 3;
+    public static final int roller_motor = 7; //roller motor PWM port
     
     public static final int j1port = 1;
     public static final int j2port = 2;
