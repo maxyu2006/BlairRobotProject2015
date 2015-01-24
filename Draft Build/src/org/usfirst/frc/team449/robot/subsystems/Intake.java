95feb4d27f0c8c13dfd7d5dc9d46b11fa5a1fa7f
AliAnwar7477 <alia7477@gmail.com>
2015-01-24 17:30:14 -0500

    Revert "Worked on Intake."
    This reverts commit 04b77ec29f00a5ff3eaf3d8dc294364156b599a2.

--- Draft Build/src/org/usfirst/frc/team449/robot/subsystems/Intake.java
+++ Draft Build/src/org/usfirst/frc/team449/robot/subsystems/Intake.java
@@ -1,8 +1,5 @@
 package org.usfirst.frc.team449.robot.subsystems;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -10,50 +7,12 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Intake extends Subsystem {
     
-	// Intake member variables 
-	private DigitalInput leftLimSwitch, rightLimSwitch;
-    private Solenoid intakeLeftSol, intakeRightSol;
-    private Victor leftArmMotor, rightArmMotor;
-    
-	// Intake constructor 
-	public Intake(){
-		
-	}
-	
-    public void initDefaultCommand() {
-    	
-    }
-    
-    // Is this meant to be a boolean? Is the boolean meant to confirm that the motor state was successfully changed?
-	public boolean setMotorState(){
-		
-		//default return statement
-		return false;
-	}
-	
-	public void toggleMotor(){
-		
-	}
-	
-	public void toggleMotorDir(){
-		
-	}
-	
-	public void setArms(boolean in){
-		
-	}
-	
-	public boolean isArmOpen(){
-		
-		//default return statement
-		return false;
-	}
-	
-	public boolean getSwitchState(){
-		
-		//default return statement
-		return false;
-	}
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
 
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
 }
 
