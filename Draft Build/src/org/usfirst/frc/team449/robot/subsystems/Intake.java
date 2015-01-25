59710f861835e25475eaaf817572af9934f45b5c
emjin <emmaljin@yahoo.com>
2015-01-25 14:59:49 -0500

    Wrote the getters and setters for intake
    What the summary says

--- Draft Build/src/org/usfirst/frc/team449/robot/subsystems/Intake.java
+++ Draft Build/src/org/usfirst/frc/team449/robot/subsystems/Intake.java
@@ -23,67 +23,76 @@ public class Intake extends Subsystem {
     private final Victor rightArmMotor;
     
     private boolean isArmOpen;
+    private boolean isMotorOn;
     
 	// Intake constructor 
-	public Intake(RobotMap config){
+	public Intake(RobotMap config) {
     	leftLimSwitch 	= new DigitalInput(config.INTAKE_LEFT_LIMIT);
     	rightLimSwitch 	= new DigitalInput(config.INTAKE_RIGHT_LIMIT);
     	
     	leftArmMotor 	= new Victor(config.INTAKE_LEFT_MOTOR);
     	rightArmMotor 	= new Victor(config.INTAKE_RIGHT_MOTOR);
     	
-    	intakeLeftSol  = new Solenoid(config.ELEVATOR_LEFT_SOLENOIDS[0], config.ELEVATOR_LEFT_SOLENOIDS[1]);
-    	intakeRightSol = new Solenoid(config.ELEVATOR_RIGHT_SOLENOIDS[0], config.ELEVATOR_RIGHT_SOLENOIDS[1]);
+    	intakeLeftSol   = new Solenoid(config.ELEVATOR_LEFT_SOLENOIDS[0], config.ELEVATOR_LEFT_SOLENOIDS[1]);
+    	intakeRightSol  = new Solenoid(config.ELEVATOR_RIGHT_SOLENOIDS[0], config.ELEVATOR_RIGHT_SOLENOIDS[1]);
     	
     	isArmOpen = true;
+    	isMotorOn = false;
 	}
 	
-    public void initDefaultCommand() {
-    	
-    }
-    
-    // Is this meant to be a boolean? Is the boolean meant to confirm that the motor state was successfully changed?
-	public boolean setMotorState(){
-		
-		//default return statement
-		return false;
+	/**
+	 * Set the motor state 
+	 */
+	public void setMotorState(boolean newState) {
+		isMotorOn = newState;
 	}
 	
 	/**
 	 * Toggles the on/off state of the motors.
 	 */
-	public void toggleMotor(){
-		
+	public void toggleMotor() {
+		isMotorOn = !isMotorOn;
 	}
 	
 	/**
 	 * Toggles the direction of the motors.
 	 */
-	public void toggleMotorDir(){
-		
+	public void toggleMotorDir() {
+		leftArmMotor.set(-leftArmMotor.get());
+		rightArmMotor.set(-rightArmMotor.get());
 	}
 	
 	/**
 	 * Toggles the open/closed state of the arms.
 	 */
-	public void toggleArms(){
-
+	public void toggleArms() {
+		isArmOpen = !isArmOpen;
 	}
 	
 	/**
 	 * Returns true if the arms are open, false otherwise.
 	 * @return isArmOpen - A boolean that is true if the arms are open, false otherwise.
 	 */
-	public boolean isArmOpen(){
+	public boolean isArmOpen() {
 		
 		return isArmOpen;
 	}
-	
-	public boolean getSwitchState(){
-		
-		//default return statement
-		return false;
+
+	/**
+	 * @return left limit switch's state
+	 */
+	public boolean getLeftSwitchState() {
+		return leftLimSwitch.get();
 	}
 
+	/**
+	 * @return right limit switch's state
+	 */
+	public boolean getRightSwitchState() {
+		return rightLimSwitch.get();
+	}
+
+    public void initDefaultCommand() {	
+    }
 }
 
