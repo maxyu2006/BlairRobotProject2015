ba610888a88c88ae7dfddbe5e100f526048d4e7e
AliAnwar7477 <alia7477@gmail.com>
2015-01-25 14:07:45 -0500

    Additional RobotMap re-formatting.

--- Draft Build/src/org/usfirst/frc/team449/robot/RobotMap.java
+++ Draft Build/src/org/usfirst/frc/team449/robot/RobotMap.java
@@ -11,41 +11,92 @@ public final class RobotMap {
 	 * ELEVATOR CONSTANTS
 	 */
 	
+	/**
+	 * Channel for the elevator's top limit switch.
+	 */
 	public final int ELEVATOR_TOP_LIMIT;
 	
+	/**
+	 * Channel for the elevator's bottom limit switch.
+	 */
 	public final int ELEVATOR_BOTTOM_LIMIT;
 	
+	/**
+	 * Channel for the elevator's left limit switch.
+	 */
 	public final int ELEVATOR_LEFT_LIMIT;
 	
+	/**
+	 * Channel for the elevator's right limit switch.
+	 */
 	public final int ELEVATOR_RIGHT_LIMIT;
 	
+	/**
+	 * Channels for the elevator's left solenoids.
+	 */
+	public final int[] ELEVATOR_LEFT_SOLENOIDS;
+	
+	/**
+	 * Channels for the elevator's right solenoids.
+	 */
 	public final int[] ELEVATOR_RIGHT_SOLENOIDS;
 	
+	/**
+	 * Channels for the elevator's brake solenoids.
+	 */
 	public final int[] ELEVATOR_BRAKE_SOLENOIDS;
-	
+
+	/**
+	 * Channel A for the elevator's encoder.
+	 */
 	public final int ELEVATOR_ENCODER_CHANNEL_A;
 	
+	/**
+	 * Channel B for the elevator's encoder.
+	 */
 	public final int ELEVATOR_ENCODER_CHANNEL_B;
 	
 	/**
 	 * DRIVE CONSTANTS
 	 */
 	
+	/**
+	 * Channels for the drive's left motors.
+	 */
 	public final int[] DRIVE_LEFT_MOTORS = {0,0,0};//somebody should give this real values
 	
+	/**
+	 * Channels for the drive's right motors.
+	 */
 	public final int[] DRIVE_RIGHT_MOTORS = {0,0,0};//somebody should give this real values
 	
 	/**
 	 * INTAKE CONSTANTS
 	 */
 	
+	/**
+	 * Channel for the intake's left limit switch.
+	 */
 	public final int INTAKE_LEFT_LIMIT;
 	
+	/**
+	 * Channel for the intake's right limit switch.
+	 */
 	public final int INTAKE_RIGHT_LIMIT;
 	
+	/**
+	 * Channel for the intake's left motor.
+	 */
 	public final int INTAKE_LEFT_MOTOR;
 	
+	/**
+	 * Channel for the intake's right motor.
+	 */
 	public final int INTAKE_RIGHT_MOTOR;
+	
+	/**
+	 * CONTROLLER PORTS
+	 */
 	
 	/**
 	 * Joystick port for Joystick 1
