71e3c10ab9c91e2ce065b4c46bd22f6f586ac3a0
anbarth <anna2nsb@gmail.com>
2015-01-25 13:44:08 -0500

    I added two spaces to Drive
    They are important spaces. Please don't delete.

--- Draft Build/src/org/usfirst/frc/team449/robot/subsystems/Drive.java
+++ Draft Build/src/org/usfirst/frc/team449/robot/subsystems/Drive.java
@@ -34,6 +34,8 @@ public class Drive extends Subsystem {
 		}
 		
 	}
+
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
