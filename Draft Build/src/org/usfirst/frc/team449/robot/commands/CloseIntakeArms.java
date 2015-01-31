443c87e1663fc77f5742ca1c9ba8f6a60012baf6
AliAnwar7477 <alia7477@gmail.com>
2015-01-31 13:16:15 -0500

    Fixed CloseIntakeArms.

--- Draft Build/src/org/usfirst/frc/team449/robot/commands/CloseIntakeArms.java
+++ Draft Build/src/org/usfirst/frc/team449/robot/commands/CloseIntakeArms.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team449.robot.commands;
 
+import org.usfirst.frc.team449.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -7,22 +9,37 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class CloseIntakeArms extends Command {
 
+	private boolean isCommandFinished;
+	
     public CloseIntakeArms() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        requires(Robot.intake);
+        
+        isCommandFinished = false;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
+    	// If the intake arms are already closed, this command is finished.
+    	if(!Robot.intake.areArmsOpen())
+    		isCommandFinished = true;
+    	
+    	// If the intake arms are open, close them.
+    	else
+    		Robot.intake.toggleArms();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
+    	// If the intake arms are closed, this command is finished.
+    	if(!Robot.intake.areArmsOpen())
+    		isCommandFinished = true;
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isCommandFinished;
     }
 
     // Called once after isFinished returns true
