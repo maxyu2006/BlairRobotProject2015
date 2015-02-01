a2fb10d8cc67a08f958a8ed01d7cbfe578d4f8bc
emjin <emmaljin@yahoo.com>
2015-02-01 16:23:21 -0500

    Revert "Small change in MoveDirect: activates the brake after finishing"
    This reverts commit 99ba2873c09cad6d3c73c063e48752a6d9efea6b.

--- Draft Build/src/org/usfirst/frc/team449/robot/commands/MoveDirect.java
+++ Draft Build/src/org/usfirst/frc/team449/robot/commands/MoveDirect.java
@@ -42,7 +42,6 @@ public class MoveDirect extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.elevator.activateBrake();
     }
 
     // Called when another command which requires one or more of the same
