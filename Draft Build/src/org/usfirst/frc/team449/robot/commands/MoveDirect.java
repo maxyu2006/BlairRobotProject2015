99ba2873c09cad6d3c73c063e48752a6d9efea6b
emjin <emmaljin@yahoo.com>
2015-02-01 16:19:36 -0500

    Small change in MoveDirect: activates the brake after finishing

--- Draft Build/src/org/usfirst/frc/team449/robot/commands/MoveDirect.java
+++ Draft Build/src/org/usfirst/frc/team449/robot/commands/MoveDirect.java
@@ -42,6 +42,7 @@ public class MoveDirect extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.elevator.activateBrake();
     }
 
     // Called when another command which requires one or more of the same
