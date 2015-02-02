747b5710f59e7b4872f9e7c7890900b37056d1ac
yonip <yoyogp.p@gmail.com>
2015-02-02 15:54:23 -0800

    Javadoc for OI

--- Draft Build/src/org/usfirst/frc/team449/robot/OI.java
+++ Draft Build/src/org/usfirst/frc/team449/robot/OI.java
@@ -13,10 +13,12 @@ public class OI {
 	
 	public final Joystick joystick1;
 	public final Joystick joystick2;
+	/** the elevator control joystick */
 	public final Joystick joystick3;
 	public final Joystick joystick4;
 	public final Button elevatorUp;
 	public final Button elevatorDown;
+	/** toggles the elevator arm between open and closed */
 	public final Button elavatorArmToggle;
 	
 	public OI(RobotMap config)
@@ -33,20 +35,39 @@ public class OI {
 	}
 	
 	/**
-	 * 
-	 * @param joystick
-	 * @return
+	 * gets the location of the joystick along the x axis as a value between -1 and 1, 
+	 * where -1 is farthest to the left and 1is farthest to the right
+	 * @param joystick the joystick in question
+	 * @return the location of the joystick along the x axis, between -1 and 1
+	 * @see #getJoystickAxisY(Joystick)
+	 * @see #getJoystickAxisThrottle(Joystick)
 	 */
 	public double getJoystickAxisX(Joystick joystick)
 	{
 		return joystick.getAxis(Joystick.AxisType.kX);
 	}
 	
+	/**
+	 * gets the location of the joystick along the y axis as a value between -1 and 1, 
+	 * where -1 is farthest to the back and 1 is farthest to the front
+	 * @param joystick the joystick in question
+	 * @return the location of the joystick along the y axis, between -1 and 1
+	 * @see #getJoystickAxisX(Joystick)
+	 * @see #getJoystickAxisThrottle(Joystick)
+	 */
 	public double getJoystickAxisY(Joystick joystick)
 	{
 		return joystick.getAxis(Joystick.AxisType.kY);
 	}
 	
+	/**
+	 * gets the value of the throttle between -1 and 1, where -1 is farthest from the joystick
+	 * and 1is closest to it
+	 * @param joystick the joystick in question
+	 * @return the value of the throttle between -1 and 1
+	 * @see #getJoystickAxisX(Joystick)
+	 * @see #getJoystickAxisY(Joystick)
+	 */
 	public double getJoystickAxisThrottle(Joystick joystick)
 	{
 		return joystick.getAxis(Joystick.AxisType.kThrottle);
