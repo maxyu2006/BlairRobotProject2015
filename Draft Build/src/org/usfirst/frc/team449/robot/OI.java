1c1bd7d04683be1a480642e6f06fbb206ea4ff78
hazheng <hazheng@mbhs.edu>
2015-02-01 22:14:36 -0500

    merged drive dev

--- Draft Build/src/org/usfirst/frc/team449/robot/OI.java
+++ Draft Build/src/org/usfirst/frc/team449/robot/OI.java
@@ -10,26 +10,69 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+    //// CREATING BUTTONS
+    // One type of button is a joystick button which is any button on a joystick.
+    // You create one by telling it which joystick it's on and which button
+    // number it is.
+    // Joystick stick = new Joystick(port);
+    // Button button = new JoystickButton(stick, buttonNumber);
+    
+    // There are a few additional built in buttons you can use. Additionally,
+    // by subclassing Button you can create custom triggers and bind those to
+    // commands the same as any other Button.
+    
+    //// TRIGGERING COMMANDS WITH BUTTONS
+    // Once you have a button, it's trivial to bind it to a button in one of
+    // three ways:
+    
+    // Start the command when the button is pressed and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenPressed(new ExampleCommand());
+    
+    // Run the command while the button is being held down and interrupt it once
+    // the button is released.
+    // button.whileHeld(new ExampleCommand());
+    
+    // Start the command when the button is released  and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenReleased(new ExampleCommand());
 	
-	public final Joystick joystick1;
-	public final Joystick joystick2;
-	public final Joystick joystick3;
-	public final Joystick joystick4;
-	public final Button elevatorUp;
-	public final Button elevatorDown;
-	public final Button elavatorArmToggle;
+	public final Joystick[] joysticks = new Joystick[4];
+	
+	/**
+	 * the button
+	 */
+	public final JoystickButton driveManualToggle;
 	
 	public OI(RobotMap config)
 	{
-		joystick1 = new Joystick(config.JOYSTICK_1);
-		joystick2 = new Joystick(config.JOYSTICK_2);
-		joystick3 = new Joystick(config.JOYSTICK_3);
-		joystick4 = new Joystick(config.JOYSTICK_4);
+		joysticks[0] = new Joystick(config.JOYSTICK_0);
+		joysticks[1] = new Joystick(config.JOYSTICK_1);
+		joysticks[2] = new Joystick(config.JOYSTICK_2);
+		joysticks[3] = new Joystick(config.JOYSTICK_3);
 		elevatorUp = new JoystickButton(joystick3, config.ELEVATOR_UP_BUTTON);
 		elevatorDown = new JoystickButton(joystick3, config.ELEVATOR_DOWN_BUTTON);
 		elavatorArmToggle = new JoystickButton(joystick3, config.ELEVATOR_ARMS_TOGGLE_BUTTON);
 		
 		
+	}
+	
+	/**
+	 * get the axis assigned to the left side of the drive
+	 * @return a double between -1 and 1
+	 */
+	public double getDriveAxis1()
+	{
+		return this.joysticks[0].getAxis(Joystick.AxisType.kY);
+	}
+	
+	/**
+	 * get the axis assigned to the right side of the drive
+	 * @return a double between -1 and 1
+	 */
+	public double getDriveAxis2()
+	{
+		return this.joysticks[1].getAxis(Joystick.AxisType.kY);
 	}
 	
 	/**
