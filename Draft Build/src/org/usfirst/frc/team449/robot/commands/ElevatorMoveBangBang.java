c858c81217f51e309263d188a54276fbdce57259
yonip <yoni.pedersen@gmail.com>
2015-03-28 14:58:42 -0400

    Nothing.

--- Draft Build/src/org/usfirst/frc/team449/robot/commands/ElevatorMoveBangBang.java
+++ Draft Build/src/org/usfirst/frc/team449/robot/commands/ElevatorMoveBangBang.java
@@ -16,46 +16,46 @@ public class ElevatorMoveBangBang extends Command {
 	/**
 	 * @param position the desired position in inches
 	 */
-    public ElevatorMoveBangBang(double position) {
-        requires(Robot.elevator);
-        setpoint = position;
-        
-        System.out.println("Elevator Move Bang Bang Initialized");
+	public ElevatorMoveBangBang(double position) {
+		requires(Robot.elevator);
+		setpoint = position;
 
-    }
+		System.out.println("Elevator Move Bang Bang Initialized");
 
-    protected void initialize() {
-        Robot.elevator.releaseBrake();
-        throttle = Math.copySign(throttle_lim, (Robot.elevator.getEncoderPosition()-setpoint));
-        Robot.elevator.disablePID();
-    }
+	}
 
-    protected void execute() {
-    	Robot.elevator.setMotorManual(throttle);
-//    	System.out.println("Set throttle to " + throttle + " at " + Robot.elevator.getEncoderPosition());
-    }
+	protected void initialize() {
+		Robot.elevator.releaseBrake();
+		throttle = Math.copySign(throttle_lim, (Robot.elevator.getEncoderPosition()-setpoint));
+		Robot.elevator.disablePID();
+	}
 
-    protected boolean isFinished() {
-    	if((throttle<0 && Robot.elevator.isTouchingTop()) || (throttle>0 && Robot.elevator.isTouchingBottom()))
-    	{
-    		System.out.println("Bang Bang: Elevator Limit Reached");
-    		return true;
-    	}
-    	if((throttle<0 && Robot.elevator.getEncoderPosition()>setpoint) || (throttle>0 && Robot.elevator.getEncoderPosition()<setpoint))
-    	{
-    		System.out.println("Bang Bang: Setpoint Reached \t"+Robot.elevator.getEncoderPosition()+" "+setpoint);
-    		return true;
-    	}
-    	return false;
-    }
-    
-    protected void end() {
-    	Robot.elevator.setMotorManual(0);
-    	Robot.elevator.activateBrake();
-    	new ElevatorMoveDefault();
-    	Robot.elevator.setHeight(setpoint);
-    }
+	protected void execute() {
+		Robot.elevator.setMotorManual(throttle);
+		//    	System.out.println("Set throttle to " + throttle + " at " + Robot.elevator.getEncoderPosition());
+	}
 
-    protected void interrupted() {
-    }
+	protected boolean isFinished() {
+		if((throttle<0 && Robot.elevator.isTouchingTop()) || (throttle>0 && Robot.elevator.isTouchingBottom()))
+		{
+			System.out.println("Bang Bang: Elevator Limit Reached");
+			return true;
+		}
+		if((throttle<0 && Robot.elevator.getEncoderPosition()>setpoint) || (throttle>0 && Robot.elevator.getEncoderPosition()<setpoint))
+		{
+			System.out.println("Bang Bang: Setpoint Reached \t"+Robot.elevator.getEncoderPosition()+" "+setpoint);
+			return true;
+		}
+		return false;
+	}
+
+	protected void end() {
+		Robot.elevator.setMotorManual(0);
+		Robot.elevator.activateBrake();
+		new ElevatorMoveDefault();
+		Robot.elevator.setHeight(setpoint);
+	}
+
+	protected void interrupted() {
+	}
 }
